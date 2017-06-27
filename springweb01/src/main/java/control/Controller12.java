997d5e0098ef645aaa5e4123a529943a2b057e50
SeungMin Kim <k0102027@naver.com>
2017-06-27 09:46:40 +0900

    .

--- springweb01/src/main/java/control/Controller12.java
+++ springweb01/src/main/java/control/Controller12.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/control/controller12/")
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class Controller12 {
 
   @RequestMapping("ok1")
-  public void ok1(String name,int age, boolean working, HttpServletResponse res) throws Exception {
+  public void ok1(String name, int age, boolean working, HttpServletResponse res) throws Exception {
     res.setContentType("text/plain;charset=UTF-8");
     PrintWriter out = res.getWriter();
     out.println("ok1()");
@@ -23,4 +24,33 @@ public class Controller12 {
     out.printf("age: %d\n", age);
     out.printf("working: %b\n", working);
   }
+  
+  @RequestMapping("ok2")
+  public void ok2(
+      @RequestParam(name="name")String n, 
+      @RequestParam(name="age")int a, 
+      @RequestParam(name="working")boolean w, HttpServletResponse res) throws Exception {
+    res.setContentType("text/plain;charset=UTF-8");
+    PrintWriter out = res.getWriter();
+    out.println("ok2()");
+    
+    out.printf("name: %s\n", n);
+    out.printf("age: %d\n", a);
+    out.printf("working: %b\n", w);
+  }
+  
+  @RequestMapping("ok3")
+  public void ok3(
+      @RequestParam(name="name", required=false)String n, 
+      @RequestParam(name="age", required=false)int a, 
+      @RequestParam(name="working", required=false)boolean w, HttpServletResponse res) throws Exception {
+    res.setContentType("text/plain;charset=UTF-8");
+    PrintWriter out = res.getWriter();
+    out.println("ok3()");
+    
+    out.printf("name: %s\n", n);
+    out.printf("age: %d\n", a);
+    out.printf("working: %b\n", w);
+  }
+  
 }
